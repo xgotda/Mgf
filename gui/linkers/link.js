7c6d7aa3d858579ef51b4a8fa325343016e7bc72
xgotda <dagmara.gotlib@gu.se>
2018-11-22 13:38:41 +0100

    Electron file browsing now opens file correctrly.

--- gui/linkers/link.js
+++ gui/linkers/link.js
@@ -2,8 +2,8 @@ function do_process(){
 	const {PythonShell} = require('python-shell')
 	const path = require("path")
 
-	
-	var fileRead = document.getElementById('fileRead').value
+
+	var fileRead = document.getElementById('fileRead').files[0].path
 	//document.getElementById('fileRead').value = ''
 	var fileWrite = document.getElementById('fileWrite').value
 	//document.getElementById('fileWrite').value = ''
@@ -11,7 +11,7 @@ function do_process(){
 	document.getElementById('aTolerance').value = '0.02'
 
 	var options = {
-		scriptPath : path.join(__dirname, '../engine/'),	
+		scriptPath : path.join(__dirname, '../engine/'),
 		args : [fileRead, fileWrite, aTolerance]
 	}
 
@@ -20,7 +20,7 @@ function do_process(){
 	// mgfprocess.end(function(err, code, message){
 	// 	swal("Done!", err + '\n' + options.args + '\n '+ code + '\n\n ' + message)
 	// })
-	
+
 	mgfprocess.on('message', function(message){
 		swal(message)
 	})
