188458dc74dedbd89f47a8bf00875cb7a6372af1
xgotda <dagmara.gotlib@gu.se>
2018-11-27 14:16:45 +0100

    Minor cleanup.

--- fileio.py
+++ fileio.py
@@ -8,39 +8,38 @@ Created on Wed Oct 10 12:06:52 2018
 
 from IonClass import Ions
 from helperMethods import *
-import staticVariables as sV
+#import staticVariables as sV
 
 
 #  Variables
-fileRead = 'mgfFiles/QEHF_180716_15.mgf'
+fileRead = 'mgfFiles/small.mgf'
 # 'smallProb.mgf'
 # 'QEHF_180716_15.mgf'
 # 'small.mgf'
-# 'Text.txt'
 fileWrite = 'sec.txt'
 
 #  oxo = oxonium (glycan)
 oxo_ppm = 5
 findOxo = [204.08667, 274.0921, 366.1395]
+oxo_tolPairs = ppm_tolerance(findOxo, oxo_ppm)
+
 #  pp = peptide (larger fragment)
 pp_ppm = 5
 findPP = [1786.9487, 1990.0281, 2136.086]
-
-oxo_tolPairs = ppm_tolerance(findOxo, oxo_ppm)
 pp_tolPairs = ppm_tolerance(findPP, pp_ppm)
 
+# cm = (doubly or triply ) charged mass
 cm = []
 for f in findPP:
     for n in range(2, 4):
         cm.append(chargedMassVar(f, n))
 
-print(str(oxo_tolPairs))
-print(str(pp_tolPairs))
-
 cm_tolPairs = ppm_tolerance(cm, pp_ppm)
 
 mz_tolPairs = cm_tolPairs.copy()
 mz_tolPairs.extend(pp_tolPairs)
+print(str(oxo_tolPairs))
+print(str(pp_tolPairs))
 print(mz_tolPairs)
 
 
@@ -65,7 +64,7 @@ def ProcessMgf():
                             elif 'PEPMASS' in line:
                                 newIon.pepmass.frLine(line.split('=')[1])
                             elif 'CHARGE' in line:
-                                newIon.charge = int(line.split('=')[1][0])
+                                newIon.charge = int(stripLine(line)[0])
                             elif 'RTINSECONDS' in line:
                                 newIon.RT = stripLine(line)
                             elif 'SCANS' in line:
