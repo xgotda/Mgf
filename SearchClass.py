48daa700e0e28be8b63e8949ba6c92da196b9405
xgotda <dagmara.gotlib@gu.se>
2018-12-10 11:55:30 +0100

    Finds correct intensity for peptides considering isotopes. isoExists moved here.

--- SearchClass.py
+++ SearchClass.py
@@ -2,7 +2,7 @@
 Created on Tue Nov 27 12:56:27 2018
 
 @author: xgotda
-"""
+""" 
 
 import PeptideClass as pc
 from helperMethods import *
@@ -13,9 +13,9 @@ class DoSearch:
 
     def __init__(self, glycans=[], glycan_ppm=0,
                  peptides=[], peptide_ppm=0):
-        self.oxoList = glycans
+        self.oxoList = glycans # glycans.sort()
         self.oxo_ppm = glycan_ppm
-        self.ppList = peptides
+        self.ppList = peptides # peptides.sort()
         self.pp_ppm = peptide_ppm
         self.searchList = []
         self.initGlycans()
@@ -29,18 +29,14 @@ class DoSearch:
 
     def initPeptides(self):
         for p in self.ppList:
-            aPep = pc.FindPep(p,
-                              calcTol(p, self.pp_ppm),
-                              pc._P)
+            aPep = pc.FindPep(p, calcTol(p, self.pp_ppm), pc._P)
             self.searchList.append(aPep)
 
     def initPotentials(self):
         for p in self.ppList:
             for n in range(2, 4):
                 c = chargedMassVar(p, n)
-                aPotential = pc.FindMcPep(c,
-                                          calcTol(c, self.pp_ppm),
-                                          n, p)
+                aPotential = pc.FindMcPep(p, c, calcTol(c, self.pp_ppm), n)
                 self.searchList.append(aPotential)
 
     def search(self, currVals, ion):
@@ -53,7 +49,43 @@ class DoSearch:
                     break
             elif s.ptype == pc._P:
                 if compare(s.mz, curr_mz, s.tol):
+                    if self.file:  # TODO: Better error handling here
+                        isoIntensity = self.isoExists(curr_mz, s)
+                        if isoIntensity:
+                        # if the first iso exists, check for second and save max
+                            isoIntensity_2 = self.isoExists(curr_mz+1, s, 2)
+                            currVals[1] = max(currVals[1],
+                                              isoIntensity, isoIntensity_2)
                     ion.addFragment(s.mz, currVals)
-                break
-            else:
+                    # Check for potentials!
+                    break
+            elif s.ptype == pc._M:
+                # if compare(s.mz, curr_mz, s.tol):
+                #     if self.file:  # TODO: Better error handling here
+                #         isoIntensity = self.isoExists(curr_mz, s)
+                #         if isoIntensity:
+                #         # if the first iso exists, check for second and save max
+                #             isoIntensity_2 = self.isoExists(curr_mz, s, 2)
+                #             currVals[1] = max(currVals[1],
+                #                               isoIntensity, isoIntensity_2)
+                #     #ion.addFragment(s.mz, currVals)
+                #     break
                 pass
+
+    def isoExists(self, mz, searchP, depth=1):
+        ''' Checks if the n'th line (depth) in the file is
+            an isotope of mz. Uses searchP (a Peptide object)
+            for charge type (chtype).
+            @return: The intensity of the isotope if found and
+                    zero if no isotope is found.
+            @rtype: float '''
+        toReturn = 0
+        nextLine = readXlines(self.file, depth)
+        if 'END' not in nextLine:
+            nextVals = pepLine(nextLine)
+            if isChargedVar(mz, nextVals[0], searchP.chtype):
+                toReturn = nextVals[1]
+        return toReturn
+
+    
+        
