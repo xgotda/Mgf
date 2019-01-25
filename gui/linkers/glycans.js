c50041abff55a8a7a87f67017eefd8d33ef9cc7a
xgotda <dagmara.gotlib@gu.se>
2019-01-25 13:36:03 +0100

    Add glycan now with multiple values to select. Not finished.

--- gui/linkers/glycans.js
+++ gui/linkers/glycans.js
@@ -5,7 +5,13 @@ const {ipcRenderer} = require('electron');
 // Send value to main when clicked 'Enter'
 const glycanForm = document.getElementById('glycanForm')
 glycanForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const glycan = document.querySelector('#glycan').value;
-  ipcRenderer.send('glycan:add', glycan);
+  e.preventDefault()
+  var glycan = []
+  for(var i = 0; i < glycanForm.elements.length-1; i++){
+    val = glycanForm[i]
+    if ((val.type == "checkbox") && (val.checked)){
+      glycan.push(val.value)
+    }
+  }
+  ipcRenderer.send('glycan:add', glycan)
 })
